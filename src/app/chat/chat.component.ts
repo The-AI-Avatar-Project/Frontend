eacc84b266e6731f3f92534ee9facc47f2863a5c
Sil-Rei <silas.reiling@hotmail.de>
2025-06-08 17:59:37 +0200

    added poster image and back button to chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -2,10 +2,12 @@ import { Component, inject, signal } from '@angular/core';
 import { ChatWindowComponent } from './ui/chat-window/chat-window.component';
 import { AvatarComponent } from './ui/avatar/avatar.component';
 import { VideoStreamService } from './data-access/stream.service';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
-  imports: [ChatWindowComponent, AvatarComponent],
+  imports: [ChatWindowComponent, AvatarComponent, MatIconModule, RouterModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
 })
@@ -19,6 +21,8 @@ export class ChatComponent {
   isStreaming = this.streamService.isStreaming;
   error = this.streamService.error;
   chatLog = this.streamService.messages;
+
+  posterImage = signal<string>('dummyprof1.png');
 
   onUserInputChange(value: string) {
     this.userInput.set(value);
