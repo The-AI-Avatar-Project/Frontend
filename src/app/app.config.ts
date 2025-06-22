282a2a36c9b585ce0685db22b177205bdb7439c4
Sil-Rei <silas.reiling@hotmail.de>
2025-06-22 17:52:33 +0200

    added default lang

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,20 +11,23 @@ import { tokenInterceptor } from './auth/token.interceptor';
 import { provideTransloco } from '@jsverse/transloco';
 import { TranslocoHttpLoader } from './transloco-loader';
 
+const defaultLang = localStorage.getItem('lang') || 'en';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
 
-    provideHttpClient(withInterceptors([tokenInterceptor])), provideHttpClient(), provideTransloco({
-        config: {
-          availableLangs: ['en', 'es'],
-          defaultLang: 'en',
-          // Remove this option if your application doesn't support changing language in runtime.
-          reRenderOnLangChange: true,
-          prodMode: !isDevMode(),
-        },
-        loader: TranslocoHttpLoader
-      }),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'de'],
+        defaultLang,
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
   ],
 };
