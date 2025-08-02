d1a45164023128e49ad1fe7a82d5b9422b6a5c02
KoanDev <okan.biyikli@hotmail.de>
2025-08-02 23:45:16 +0200

    Clean up Add Toast Message

--- src/app/shared/services/room.service.ts
+++ src/app/shared/services/room.service.ts
@@ -2,6 +2,7 @@ import {inject, Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {AuthService} from '../../auth/auth.service';
 import {lastValueFrom, Observable} from 'rxjs';
+import {MessageService} from 'primeng/api';
 
 @Injectable({
   providedIn: 'root'
@@ -10,25 +11,7 @@ export class RoomService {
 
   http: HttpClient = inject(HttpClient);
   auth: AuthService = inject(AuthService);
-
-  createRoom(name: string, year: number, semester: string, icon: string) {
-    const url = 'http://localhost:8080/rooms';
-    const body = {
-      name: name,
-      year: year,
-      semester: semester,
-      icon: icon
-    }
-    const token = this.auth.getToken();
-
-    console.log(body)
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${token}`,
-    });
-
-    return this.http.post(url, body, {headers});
-  }
-
+  protected messageService: MessageService = inject(MessageService);
 
   getAllRooms() {
     return this.http.get<any[]>('http://localhost:8080/rooms');
@@ -37,7 +20,7 @@ export class RoomService {
   private readonly apiUrl = 'http://localhost:8080/users';
 
 
-  addUsersToGroup(groupId: string, userIds: string[]): Observable<void> {
+  addUsersToGroup(groupId: string, userIds: string[]): Observable<any> {
     const bearertoken = this.auth.getToken();
 
     const payload = {
@@ -50,35 +33,33 @@ export class RoomService {
       "Content-Type": "application/json"
     });
 
-    return this.http.post<void>(this.apiUrl, payload, {headers});
+    return this.http.post<any>(this.apiUrl, payload, {headers, observe: 'response'});
   }
 
 
   async createGroup(name: string, year: number, semester: string, icon: string): Promise<any> {
-    console.log("Creating Room");
     const apiUrl = `http://localhost:8080/rooms`;
     const bearertoken = this.auth.getToken();
-
     const headers = new HttpHeaders({
       Authorization: `Bearer ${bearertoken}`,
       "Content-Type": "application/json"
     });
-
     const body = {
       name: name,
       year: year,
       semester: semester,
       icon: icon
     };
-
     try {
-      const response = await lastValueFrom(
+      return await lastValueFrom(
         this.http.post(apiUrl, body, {headers})
-      );
-
-      console.log("Room created:", response);
-      return response;
-
+      ).then((response) =>
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Raum erfolgreich erstellt',
+          detail: '',
+        })
+      )
     } catch (err) {
       console.error("Fehler beim Erstellen des Raums:", err);
       throw err;
